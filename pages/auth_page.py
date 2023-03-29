6a9d0152e82005477d0680331707fe8557dc4863
val <you@ex.com>
2023-03-29 13:49:07 +0300

    11_tests

--- pages/auth_page.py
+++ pages/auth_page.py
@@ -63,3 +63,64 @@ class AuthPage(BasePage):
         self.reg.click()
     def phone_element_click(self):
         self.phone_tab_element.click()
+
+    def get_address_element(self):
+        return self.driver.find_element(*AuthLocators.REG_ADDRESS)
+    def get_first_element(self):
+        return self.driver.find_element(*AuthLocators.REG_FIRSTNAME)
+    def get_lastname(self):
+        return self.driver.find_element(*AuthLocators.REG_LASTNAME)
+    def get_region(self):
+        return self.driver.find_element(*AuthLocators.REG_REGION)
+    def get_passw_element(self):
+        return self.driver.find_element(*AuthLocators.AUTH_PASS)
+    def get_passw_confirm_element(self):
+        return self.driver.find_element(*AuthLocators.REG_PASSW_CONFIRM)
+    def get_link_confidental_element(self):
+        return self.driver.find_element(*AuthLocators.REG_LINK_CONFIDENTAL)
+    def get_reg_logo_element(self):
+        return self.driver.find_element(*AuthLocators.REG_LOGO)
+    def get_confirm_element(self):
+        return self.driver.find_element(*AuthLocators.CONFIRM_EMAIL)
+
+    def get_reg_btn(self):
+        return self.driver.find_element(*AuthLocators.REG_BTN)
+
+    def get_reg_name_error(self):
+        return self.driver.find_element(*AuthLocators.REG_ERROR_NAME)
+
+class RestorePage(BasePage):
+
+    def __init__(self, driver, timeout=5):
+        super().__init__(driver, timeout)
+        url = "https://b2c.passport.rt.ru/auth/realms/b2c/login-actions/reset-credentials?client_id=account_b2c&tab_id=LuLoWsuA3wg"
+        driver.get(url)
+        self.phone = driver.find_element(*AuthLocators.AUTH_PHONE)
+        self.email = driver.find_element(*AuthLocators.AUTH_EMAIL)
+        self.phone_tab_element = driver.find_element(*AuthLocators.PHONE_TAB_ELEMENT)
+        self.mail_tab_element = driver.find_element(*AuthLocators.MAIL_TAB_ELEMENT)
+        self.login_tab_element = driver.find_element(*AuthLocators.LOGIN_TAB_ELEMENT)
+        self.ls_tab_element = driver.find_element(*AuthLocators.LS_TAB_ELEMENT)
+        self.kapcha_element = driver.find_element(*AuthLocators.KAPCHA_ELEMENT)
+        self.reset_btn = driver.find_element(*AuthLocators.RESET_BTN)
+        self.reset_back = driver.find_element(*AuthLocators.RESET_BACK)
+
+    def get_phone_element(self):
+        return self.phone_tab_element
+    def get_mail_element(self):
+        return self.mail_tab_element
+    def get_login_element(self):
+        return self.login_tab_element
+    def get_ls_element(self):
+        return self.ls_tab_element
+    def get_login_field(self):
+        return self.phone
+
+    def get_kapcha_element(self):
+        return self.kapcha_element
+    def get_reset_btn_element(self):
+        return self.reset_btn
+
+
+
+
--- tests/test_auth_page.py
+++ tests/test_auth_page.py
@@ -3,9 +3,9 @@
 import pytest
 
 import time
-from pages.auth_page import AuthPage
+from pages.auth_page import AuthPage, RestorePage
 
-# #Тестирование формы "Авторизация"
+# #1Тестирование формы "Авторизация"
 # def test_auth_page_elements(selenium):
 #     page = AuthPage(selenium)
 #     assert page.get_phone_element().is_displayed() == True
@@ -19,7 +19,7 @@ from pages.auth_page import AuthPage
 #     assert page.get_fogot_passw_element().is_displayed() == True
 #     assert page.get_btn_element().is_displayed() == True
 
-# #Позитивная регистрация
+# #2Позитивная регистрация
 # @pytest.mark.parametrize('x', ["9959967398", "regpostt@mail.ru"], ids=["valid_phone", "valid_mail"])
 # def test_positive_auth(selenium, x):
 #    page = AuthPage(selenium)
@@ -28,7 +28,7 @@ from pages.auth_page import AuthPage
 #    page.btn_click()
 #    assert page.get_relative_link() == '/account_b2c/page'
 
-# #Негативная регистрация с неверным паролем
+# #3Негативная регистрация с неверным паролем
 # @pytest.mark.parametrize('x', ["9959967398", "regpostt@mail.ru"], ids=["valid_phone", "valid_mail"])
 # def test_negative_auth_wrong_passw(selenium, x):
 #     page = AuthPage(selenium)
@@ -39,7 +39,7 @@ from pages.auth_page import AuthPage
 #     assert page.get_error_massage() == "Неверный логин или пароль"
 
 
-# #Негативная регистрация с неверным логином
+# #4Негативная регистрация с неверным логином
 # @pytest.mark.parametrize('x', ["9959967399", "regpost@mail.ru"], ids=["invalid_phone", "invalid_mail"])
 # def test_negative_auth_wrong_passw(selenium, x):
 #     page = AuthPage(selenium)
@@ -49,14 +49,85 @@ from pages.auth_page import AuthPage
 #     time.sleep(1)
 #     assert page.get_error_massage() == "Неверный логин или пароль"
 
-# # По умолчанию выбрана форма авторизации по телефону
-# def test_registration_page_elements(selenium):
+# #5По умолчанию выбрана форма авторизации по телефону
+# def test_active_elements(selenium):
 #    page  = AuthPage(selenium)
 #    assert page.get_phone_element().get_attribute("class") == "rt-tab rt-tab--small rt-tab--active"
 #    assert page.get_phone_element().text == 'Телефон'
 #    assert page.get_mail_element().get_attribute("class") != "rt-tab rt-tab--small rt-tab--active"
 
-# def test_registration(selenium):
-#    page = AuthPage(selenium)
-#    page.reg_click()
-#    assert page.get_relative_link() == '/account_b2c/page'
+# #6Смена таб выбора при введении данных
+# @pytest.mark.parametrize('x', ["9959967398", "regpostt@mail.ru", "gfhgfghf2"], ids=["valid_phone", "valid_mail", "login"])
+# def test_check_change_active_element(selenium, x):
+#     page = AuthPage(selenium)
+#     page.enter_email(x)
+#     page.enter_pass("Skillfactory1007")
+#     time.sleep(3)
+#     assert page.get_login_element().get_attribute("class") or page.get_phone_element().get_attribute("class") or page.get_mail_element().get_attribute("class") == "rt-tab " \
+#                                                                                                              "rt-tab--small rt-tab--active"
+
+
+# #7Тестирование формы "Восстановление пароля"
+# def test_auth_page_elements(selenium):
+#     page = RestorePage(selenium)
+#     assert page.get_phone_element().is_displayed() == True
+#     assert page.get_mail_element().is_displayed() == True
+#     assert page.get_login_element().is_displayed() == True
+#     assert page.get_ls_element().is_displayed() == True
+#     assert page.get_login_field().is_displayed() == True
+#     # assert page.get_kapcha_element().get_attribute("alt") == "Captcha"
+#     assert page.get_kapcha_element().is_displayed() == True
+#     assert page.get_reset_btn_element().is_displayed() == True
+#     assert page.reset_back.is_displayed() == True
+
+# #8Тестирование формы "Регистрация"
+# def test_auth_page_elements(selenium):
+#     page = AuthPage(selenium)
+#     page.reg_click()
+#     assert page.get_first_element().is_displayed() == True
+#     assert page.get_lastname().is_displayed() == True
+#     assert page.get_address_element().is_displayed() == True
+#     assert page.get_region().is_displayed() == True
+#     assert page.get_passw_element().is_displayed() == True
+#     assert page.get_passw_confirm_element().is_displayed() == True
+#     assert page.get_link_confidental_element().is_displayed() == True
+#     assert page.get_reg_logo_element().is_displayed() == True
+
+# #9Тестирование позитивной регистрации
+# def test_positive_registration(selenium):
+#     page = AuthPage(selenium)
+#     page.reg_click()
+#     page.get_first_element().send_keys("Рааа")
+#     page.get_lastname().send_keys("ааф")
+#     page.get_address_element().send_keys("example@email.ru")
+#     page.get_passw_element().send_keys("Superqwerty123")
+#     page.get_passw_confirm_element().send_keys("Superqwerty123")
+#     page.get_reg_btn().click()
+#     time.sleep(5)
+#     assert page.get_confirm_element().text == "Подтверждение email"
+
+# #10Позитивное тестирование поля имя(минимум 2 символа состоящих из букв кириллицы или знака тире (-))
+# @pytest.mark.parametrize('x', ["фф", "ааа", "фф-", "ааааааааааааааааааааааааааааа"])
+# def test_positive_registration(selenium, x):
+#     page = AuthPage(selenium)
+#     page.reg_click()
+#     page.get_first_element().send_keys(x)
+#     page.get_lastname().send_keys("ааф")
+#     page.get_address_element().send_keys("example@email.ru")
+#     page.get_passw_element().send_keys("Superqwerty123")
+#     page.get_passw_confirm_element().send_keys("Superqwerty123")
+#     page.get_reg_btn().click()
+#     assert page.get_confirm_element().text == "Подтверждение email"
+
+
+# #11Негативное тестирование поля Имя
+# def test_negative_registration(selenium):
+#     page = AuthPage(selenium)
+#     page.reg_click()
+#     name = ["1", "", "@@а", "-а", "а", "аааааааааааааааааааааааааааааа", "f"]
+#     for i in name:
+#         page.get_first_element().send_keys(i)
+#         page.get_reg_btn().click()
+#         assert page.get_reg_name_error().text == "Необходимо заполнить поле кириллицей. От 2 до 30 символов."
+
+
